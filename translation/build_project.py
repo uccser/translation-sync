d1707fa24d79f3d30b7a0dfa246ba4fff5c9ff99
Jack Morgan <jackmorgannz@gmail.com>
2018-09-16 12:52:29 +1200

    Test using CURL for build API call

--- translation/build_project.py
+++ translation/build_project.py
@@ -3,15 +3,7 @@ import requests
 
 
 def build_project(project):
-    params = {
-        "key": project.crowdin_api_key,
-        "json": True,
-    }
-    response = requests.get(
-        API_URL.format(project=project.name, method="export"),
-        params=params,
-        stream=True,
-    )
-    response_data = response.json()
-    print(response_data)
-    return response_data
+    print("Triggering build of translations for {}...".format(project.name))
+    url = API_URL.format(project=project.name, method="export")
+    url += "?key={key}".format(key=project.crowdin_api_key)
+    run_shell(["curl", url])
